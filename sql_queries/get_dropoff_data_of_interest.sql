8574a250c6dec61b38032a4634bbffa947bd8034
Sungho Park <sunghopark2010@gmail.com>
2014-03-09 13:20:52 -0500

    added google map; working on visualization

--- sql_queries/get_dropoff_data_of_interest.sql
+++ sql_queries/get_dropoff_data_of_interest.sql
@@ -1,14 +1,53 @@
-﻿select
-	case when t.gender_cd = 'male' then 0 else 1 end as gender
-	, 2013 - t.birthyear as age
-	, extract(hour from t.start_time) as time_of_day
-	, to_char(extract(month from t.start_time), 'fm00') || to_char(extract(day from t.start_time), 'fm00') as "date"
+﻿drop table if exists dropoffs_of_interest;
+create table dropoffs_of_interest as
+(
+select
+	t.trip_id
+	, case when t.gender_cd = 'male' then 0 else 1 end as gender
+	, least(((2013 - t.birthyear) / 10) - 1, 6) as age
+	, extract(month from t.end_time)-6 as "month"
+	, extract(dow from t.end_time) as day_of_week
+	, extract(hour from t.end_time)::int / 3 as hour_of_day
+	, t.to_station_id
+	, case
+		when dbs.distance = 0 then 0 
+		when dbs.distance <= 1 then 1
+		when dbs.distance <= 2 then 2
+		when dbs.distance <= 3 then 3
+		when dbs.distance <= 5 then 4
+		when dbs.distance <= 10 then 5
+		when dbs.distance > 10 then 6
+	end as distance
+	, case
+		when t.duration <= 60 * 10 then 0
+		when t.duration <= 60 * 20 then 1
+		when t.duration <= 60 * 30 then 2
+		when t.duration <= 60 * 60 then 3
+		when t.duration <= 60 * 120 then 4
+		when t.duration <= 60 * 240 then 5
+		when t.duration > 60 * 240 then 6
+	end as duration		
+	, case
+		when (dbs.distance / t.duration * 3600) = 0 then 0
+		when (dbs.distance / t.duration * 3600) <= 3 then 1
+		when (dbs.distance / t.duration * 3600) <= 5 then 2
+		when (dbs.distance / t.duration * 3600) <= 7 then 3
+		when (dbs.distance / t.duration * 3600) <= 9 then 4
+		when (dbs.distance / t.duration * 3600) <= 12 then 5
+		when (dbs.distance / t.duration * 3600) > 12 then 6
+	end as speed -- miles per hour
 from
 	trips t
-left join distances_between_stations dbs on dbs.from_station_id = t.from_station_id and dbs.to_station_id = t.to_station_id
+left join
+	distances_between_stations dbs
+on
+	dbs.from_station_id = t.from_station_id and dbs.to_station_id = t.to_station_id
 where
 	t.gender_cd is not null
 	and t.birthyear is not null
 	and t.user_type_cd = 'subscriber'
-	and t.end_time between '2013-08-01'::date and '2013-09-01'::date
-	
+	and t.end_time between '2013-06-01'::date and '2013-09-01'::date
+);
+create index doi_trip_id on dropoffs_of_interest(trip_id);
+
+select gender, age, month, day_of_week as dow, hour_of_day as hod, to_station_id as station_id, distance, duration, speed from dropoffs_of_interest;
--- sql_queries/get_pickup_data_of_interest.sql
+++ sql_queries/get_pickup_data_of_interest.sql
@@ -4,27 +4,50 @@ create table pickups_of_interest as
 select
 	t.trip_id
 	, case when t.gender_cd = 'male' then 0 else 1 end as gender
-	, 2013 - t.birthyear as age
-	, extract(hour from t.start_time) as time_of_day
-	, extract(dow from t.start_time) as weekday
-	, (t.duration / 60)::Decimal(6, 2) as duration
-	, dbs.distance
-	, (dbs.distance / t.duration * 3600)::Decimal(5, 2) as velocity -- miles per hour
-	
+	, least(((2013 - t.birthyear) / 10) - 1, 6) as age
+	, extract(month from t.start_time)-6 as "month"
+	, extract(dow from t.start_time) as day_of_week
+	, extract(hour from t.start_time)::int / 3 as hour_of_day
+	, t.from_station_id
+	, case
+		when dbs.distance = 0 then 0 
+		when dbs.distance <= 1 then 1
+		when dbs.distance <= 2 then 2
+		when dbs.distance <= 3 then 3
+		when dbs.distance <= 5 then 4
+		when dbs.distance <= 10 then 5
+		when dbs.distance > 10 then 6
+	end as distance
+	, case
+		when t.duration <= 60 * 10 then 0
+		when t.duration <= 60 * 20 then 1
+		when t.duration <= 60 * 30 then 2
+		when t.duration <= 60 * 60 then 3
+		when t.duration <= 60 * 120 then 4
+		when t.duration <= 60 * 240 then 5
+		when t.duration > 60 * 240 then 6
+	end as duration		
+	, case
+		when (dbs.distance / t.duration * 3600) = 0 then 0
+		when (dbs.distance / t.duration * 3600) <= 3 then 1
+		when (dbs.distance / t.duration * 3600) <= 5 then 2
+		when (dbs.distance / t.duration * 3600) <= 7 then 3
+		when (dbs.distance / t.duration * 3600) <= 9 then 4
+		when (dbs.distance / t.duration * 3600) <= 12 then 5
+		when (dbs.distance / t.duration * 3600) > 12 then 6
+	end as speed -- miles per hour
 from
 	trips t
-left join distances_between_stations dbs on dbs.from_station_id = t.from_station_id and dbs.to_station_id = t.to_station_id
+left join
+	distances_between_stations dbs
+on
+	dbs.from_station_id = t.from_station_id and dbs.to_station_id = t.to_station_id
 where
 	t.gender_cd is not null
 	and t.birthyear is not null
 	and t.user_type_cd = 'subscriber'
-	and t.start_time between '2013-08-01'::date and '2013-09-01'::date
-
-	and t.duration is not null -- should be deleted later
+	and t.start_time between '2013-06-01'::date and '2013-09-01'::date
 );
 create index poi_trip_id on pickups_of_interest(trip_id);
 
-select max(duration), max(distance), max(velocity) from pickups_of_interest;
--- 84202, 14.29, 466.0274
-
-select * from pickups_of_interest;
+select gender, age, month, day_of_week as dow, hour_of_day as hod, from_station_id as station_id, distance, duration, speed from pickups_of_interest;
