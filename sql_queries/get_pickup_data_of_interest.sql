9469d95427ff1a92f0b0dbd3afe6d05629c4d007
zoexwang <zoe.x.wang@gmail.com>
2014-03-10 22:17:33 -0500

    change speed back

--- sql_queries/get_pickup_data_of_interest.sql
+++ sql_queries/get_pickup_data_of_interest.sql
@@ -28,13 +28,13 @@ select
 		when t.duration > 60 * 60 then 6
 	end as duration		
     , case
-		when (dbs.distance / t.duration * 3600/1609) = 0 then 0
-		when (dbs.distance / t.duration * 3600/1609) <= 4 then 1
-		when (dbs.distance / t.duration * 3600/1609) <= 6 then 2
-		when (dbs.distance / t.duration * 3600/1609) <= 8 then 3
-		when (dbs.distance / t.duration * 3600/1609) <= 10 then 4
-		when (dbs.distance / t.duration * 3600/1609) <= 12 then 5
-		when (dbs.distance / t.duration * 3600/1609) > 12 then 6
+		when (dbs.distance / t.duration * 3600) = 0 then 0
+		when (dbs.distance / t.duration * 3600) <= 4 then 1
+		when (dbs.distance / t.duration * 3600) <= 6 then 2
+		when (dbs.distance / t.duration * 3600) <= 8 then 3
+		when (dbs.distance / t.duration * 3600) <= 10 then 4
+		when (dbs.distance / t.duration * 3600) <= 12 then 5
+		when (dbs.distance / t.duration * 3600) > 12 then 6
 	end as speed -- miles per hour
 from
 	trips t
